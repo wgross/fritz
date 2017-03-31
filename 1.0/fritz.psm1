2e0b01afc9711399b319e698f147a4f99321bf44
Wolfgang Gross <github@text-mode.de>
2017-03-31 17:18:13 +0200

    initila checkin

---
+++ 1.0/fritz.psm1
@@ -0,0 +1,21 @@
+﻿function Get-Tr64description {
+    process {
+        Invoke-RestMethod -Method Get -Uri http://fritz.box:49000/tr64desc.xml
+    }
+}
+
+function Get-ServiceList {
+    begin {
+        $splattedParameters = @{
+            Namespace = @{
+                dns= "urn:delforum-org:device-1-0"
+            }
+        }
+    }
+    process {
+         Select-Xml -Xml (Get-Tr64description) -XPath "//service" @splattedParameters
+    }
+}
+
+Get-Tr64description
+Get-ServiceList
